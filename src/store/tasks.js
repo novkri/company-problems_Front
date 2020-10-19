c19f3c88d221bc64d7658a726d95c91361cb279d
novkri <nov.kri@yandex.ru>
2020-10-19 23:31:36 +0700

    task deadline fix

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -128,11 +128,8 @@ export default {
             if (error.response.status == 422) {
               if (error.response.data.errors.description) {
                 commit('setError', error.response.data.errors.description[0])
-              } else 
+              } else if (error.response.data.errors || error.response.data.errors.deadline)
                 error.response.data.errors ? commit('setError404', error.response.data.errors.deadline[0]) : commit('setError404', error.response.data.error)
-              // } else {
-              //   commit('setError404', error.response.data.errors)
-              // }
             } else {
               commit('setError404', error.response.data.message)
             }
